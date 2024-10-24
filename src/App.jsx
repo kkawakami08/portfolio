e232625c0bcdab93eb1613e94f7b6f371e7d1a9a
Kaori Kawakami <k.kawakami08@gmail.com>
2024-10-24 15:44:12 -0700

    Initial commit

---
+++ src/App.jsx
@@ -0,0 +1,11 @@
+import React from "react";
+
+const App = () => {
+  return (
+    <div className="bg-dark-green text-beige">
+      <p>HI</p>
+    </div>
+  );
+};
+
+export default App;
---
+++ tailwind.config.js
@@ -0,0 +1,16 @@
+/** @type {import('tailwindcss').Config} */
+export default {
+  content: ["./index.html", "./src/**/*.{js,ts,jsx,tsx}"],
+  theme: {
+    extend: {
+      colors: {
+        "dark-green": "#313628",
+        beige: "#DCC48E",
+        "pale-green": "#EAEFD3",
+        "light-green": "#B3C0A4",
+        tangerine: "#F96E46",
+      },
+    },
+  },
+  plugins: [],
+};
